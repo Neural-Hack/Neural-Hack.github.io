150a5e844b93fe568dfb1a600ed6c3ccf4e4e25a
MonkeyyBanana <104471645+MonkeyyBanana@users.noreply.github.com>
2024-03-11 04:41:39 -0700

    clean later pls

--- nextra.theme.tsx
+++ nextra.theme.tsx
@@ -1,19 +1,38 @@
 import Link from "next/link"
 import { Accordion, AccordionItem } from "@nextui-org/accordion";
+import { Breadcrumbs, BreadcrumbItem } from "@nextui-org/breadcrumbs";
 import type { NextraThemeLayoutProps, PageMapItem } from 'nextra'
 import { Navbar } from "./components/navbar";
 import "@/styles/globals.css";
 import { Metadata } from "next";
 import { siteConfig } from "@/config/site";
-import { fontSans } from "@/config/fonts";
-import clsx from "clsx";
+import { Item, normalizePages } from 'nextra/normalize-pages'
 import { Providers } from "./app/providers";
 import { Footer } from "./components/footer";
+import { useRouter } from "next/router";
+import { useMemo } from "react";
 
 interface SMeta {
     [fileName: string]: any;
 }
 
+export function Bread({
+    activePath
+}: {
+    activePath: Item[]
+}): JSX.Element {
+    const stripPath = activePath.filter(item => item.title !== "learn");
+
+    return (
+        <Breadcrumbs size="lg" isDisabled={true}>
+            {stripPath.map((item) => (
+                <BreadcrumbItem key={item.title}>
+                    {item.title}
+                </BreadcrumbItem>
+            ))}
+        </Breadcrumbs>
+    );
+}
 
 export const metadata: Metadata = {
     title: {
@@ -32,53 +51,84 @@ export const metadata: Metadata = {
     },
 };
 
+export const ERROR_ROUTES = new Set(['/404', '/500'])
+
+export const useFSRoute = () => {
+    const { locale = "", asPath, route } = useRouter()
+
+    return useMemo(() => {
+        // because for the 404 route `asPath` will be redirected URL and `normalizePages` will never return correct pageItem
+        const clientRoute = ERROR_ROUTES.has(route) ? route : asPath
+
+        const { pathname } = new URL(clientRoute, "https://neural-hack.github.io")
+
+        const cleanedPath = locale
+            ? pathname.replace(new RegExp(`\\.${locale}(\\/|$)`), '$1')
+            : pathname
+
+        return (
+            cleanedPath
+                .replace(/\.html$/, '')
+                .replace(/\/index(\/|$)/, '$1')
+                .replace(/\/$/, '') || '/'
+        )
+    }, [asPath, locale, route])
+}
+
+const traverseItem = (item: SMeta): JSX.Element => {
+    return (
+        <Link key={item["title"]} href={item["href"]} className="ml-5 mb-2">
+            {item["title"]}
+        </Link>
+    )
+}
+
+const traverseMeta = (me: PageMapItem): JSX.Element => {
+    let out = [] as JSX.Element[];
+    if (me.kind === "Meta") {
+        Object.keys(me.data).forEach((key) => {
+            const metaItem = me.data[key] as SMeta;
+            let pages = [] as JSX.Element[];
+
+            Object.keys(metaItem["items"]).forEach((key) => {
+                pages.push(traverseItem((metaItem["items"] as SMeta)[key]))
+            });
+
+            out.push(
+                <AccordionItem key={key} title={metaItem["title"]}>
+                    <div className="flex flex-col">
+                        {pages}
+                    </div>
+                </AccordionItem>
+            );
+        });
+    }
+
+    return <Accordion className="w-1/3 hidden md:block" showDivider={false}>{out}</Accordion>;
+}
+
 export default function Layout({ children, pageOpts }: NextraThemeLayoutProps) {
     const { pageMap } = pageOpts;
-    
-    const traverseItem = (item: SMeta): JSX.Element => {
-        return (
-            <Link key={item["title"]} href={item["href"]}>
-                {item["title"]}
-            </Link>
-        )
-    }
-
-    const traverseMeta = (me: PageMapItem): JSX.Element => {
-        let out = [] as JSX.Element[];
-        if (me.kind === "Meta") {
-            Object.keys(me.data).forEach((key) => {
-                const metaItem = me.data[key] as SMeta;
-                let pages = [] as JSX.Element[];
-
-                Object.keys(metaItem["items"]).forEach((key) => {
-                    pages.push(traverseItem((metaItem["items"] as SMeta)[key]))
-                });
-
-                out.push(
-                    <AccordionItem key={key} title={metaItem["title"]}>
-                        <div className="flex flex-col">
-                        {pages.flatMap(item => item)}
-                        </div>  
-                    </AccordionItem>
-                );
-            });
-        }
-
-        return <Accordion>{out}</Accordion>;
-    }
-
+    const { locale = "", defaultLocale } = useRouter()
+    const fsPath = useFSRoute()
+    const { activeType, activePath } = useMemo(() => normalizePages({ list: pageMap, locale, defaultLocale, route: fsPath }), [pageMap, locale, defaultLocale, fsPath])
     const firstEntry = pageMap[0].kind === 'Folder' ? pageMap[0].children : ({} as PageMapItem[]);
     const meta = firstEntry.find(item => item.kind === "Meta") as PageMapItem;
 
     return (
         <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
             <div className="relative flex flex-col h-screen">
-                <div>
-                    <Navbar />
+                <Navbar />
+                <div className="flex flex-row self-center gap-4 max-w-screen-xl w-full p-2">
                     {traverseMeta(meta)}
-                    <div style={{ border: '1px solid' }}>{children}</div>
-                    <Footer/>
+                    <div className="w-full">
+                        {Bread({activePath})}
+                        <div className="m-2">
+                            {children}
+                        </div>
+                    </div>
                 </div>
+                <Footer />
             </div>
         </Providers>
     )
