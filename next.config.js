fab48648960c20ac28c48520a53691ee1b3e3666
MonkeyyBanana <104471645+MonkeyyBanana@users.noreply.github.com>
2024-09-09 23:20:49 -0700

    took export out

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
-/**
- * @type {import('next').NextConfig}
- */
-const nextConfig = {
-  output: 'export'
-}
-
 const withNextra = require('nextra')({
   theme: './nextra.theme.tsx'
 })
 
-module.exports = withNextra(nextConfig)
+module.exports = withNextra()
